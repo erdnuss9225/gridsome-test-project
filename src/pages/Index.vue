752c6146b3c3e7ff4d850c85c7fdb3f3a56776bd
erdnuss9225 <benjamin@pussel.de>
2020-03-02 11:02:36 +0100

    Index.vue als Page angepasst zur Verwendung als Startseite

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,66 @@
 <template>
   <Layout>
+   <div v-if="$root.language == 'de' " class="teaser" v-for="articleItem in $page.allSingleData.edges[0].node.de">
+      <img :src="articleItem.teaser_image" alt="teaser Image">
+      <div class="teaser__text">
+        <h3>{{articleItem.headline}}</h3>
+        <p>{{articleItem.abstract}}</p>
+        <button class="button__link" v-on:click="$router.push({ path: articleItem.slug })">Mehr lesen</button>
+      </div>
+    </div>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+  <div v-if="$root.language == 'en' " class="teaser" v-for="articleItem in $page.allSingleData.edges[0].node.en">
+    <img :src="articleItem.teaser_image" alt="teaser Image">
+    <div class="teaser__text">
+      <h3>{{articleItem.headline}}</h3>
+      <p>{{articleItem.abstract}}</p>
+      <button class="button__link" v-on:click="$router.push({ path: articleItem.slug })">Read more</button>
+    </div>
+  </div>
 
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
 
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+query { 
+	allSingleData {
+    edges {	
+      node {
+        de {
+          headline
+          teaser_image
+          abstract
+          slug
+        }
+        en {
+          headline
+          teaser_image
+          abstract
+          slug
+        }
+      }
+    }
   }
 }
-</script>
+</page-query>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+
+
+<script>
+export default {
+
+  data() {
+        return {
+            currentLocale: "",
+            currentLocaleHelp: {
+              de: ["Mehr lesen", "Weitere Artikel"],
+              en: ["Read More", "Check out further Articles"]
+            },
+            locales: [ {id: 'en', name: 'English', more: 'Read more'}, {id: 'de', name: 'Deutsch', more: 'Mehr lesen'}]
+        }
+    },
+ 
+
 }
-</style>
+</script>

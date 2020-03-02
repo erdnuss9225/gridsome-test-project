7920deafc2c85594553bfce23651f2691d87c1a1
erdnuss9225 <benjamin@pussel.de>
2020-03-02 10:36:34 +0100

    gridsome.config.js angepasst (SiteName und Plugins)

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: "Breaking News of the Day",
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'article/**/*.md',
+        typeName: 'Article',
+        remark: {
+          // remark options
+        }
+      }
+    },
+
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'old/*.md',
+        typeName: 'singleData',
+        remark: {
+          // remark options
+        }
+      }
+    }
+
+  ],
 }
